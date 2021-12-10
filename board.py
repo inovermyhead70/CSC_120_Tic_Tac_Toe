c513ad5dd8c3d5bc2d3725b01107d3217f5df124
Tonia <toniawms70@yahoo.com>
2021-12-09 23:42:05 -0500

    Add logic for win, lose or draw.

--- board.py
+++ board.py
@@ -3,22 +3,73 @@ board = ["-", "-", "-",
          "-", "-", "-"]
          
 currentPlayer = "X"
-gameContinue = True         
+gameContinue = True  
+winner = None       
          
 def print_board():
     print(board[0] + "|" + board[1] + "|" + board[2])
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
    
+
     
-def check_mark(board):
+def player_input(board):
     global currentPlayer
-    print("It is your turn " + currentPlayer) 
-    choice = int(input("Enter a number 1-9: "))
-    if choice >= 1 and choice <= 9 and board[choice-1] == "-":
-      board[choice-1] = currentPlayer
+    
+    print("It is your turn " + currentPlayer)
+    position = int(input("Enter a number 1-9: "))
+    
+    if position >= 1 and position <= 9 and board[position-1] == "-":
+        board[position-1] = currentPlayer
     else:
-      print("Spot taken. Please choose again!")
+        print("Number invalid or position taken!")    
+        
+      
+def check_row(board):
+    global winner  
+    if board[0] == board[1] == board[2] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True  
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True        
+
+def check_column(board):
+    global winner  
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True  
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+def check_diag(board):
+    global winner  
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[6] == board[4] == board[2] and board[6] != "-":
+        winner = board[6]
+        return True
+
+def check_win():
+    if check_row(board) or check_column(board) or check_diag(board):
+        print(f"The winner is {winner}")
+        gameContinue = False
+
+def check_tie(board):
+    global gameContinue
+    if "-" not in board:
+        print_board(board)
+        print("it is a tie!")
+        gameContinue = False
+
       
 def switch_player():
     global currentPlayer
@@ -29,6 +80,8 @@ def switch_player():
        
 while gameContinue:
   print_board()
-  check_mark(board)
+  player_input(board)
+  check_win()
+  check_tie(board)
   switch_player()
   
